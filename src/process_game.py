2e369a1f331607e13473dd8404166ec685f8c647
millcityrunner <mcaray9@gmail.com>
2021-04-05 07:56:20 -0500

    adding in my changes before vaca

--- src/process_game.py
+++ src/process_game.py
@@ -6,19 +6,21 @@ from carball.decompile_replays import decompile_replay
 from carball.json_parser.game import Game
 from carball.analysis.analysis_manager import AnalysisManager
 
-# def analyzeGame():
-  # return carball.decompile_replay('example.replay')
 def analyzeGame(fname):
-  _json = decompile_replay(f"/tmp/curr_replay")
+  # _json = decompile_replay(f"/tmp/curr_replay_{fname}")
+  _json = decompile_replay(fname)
   game = Game()
   game.initialize(loaded_json=_json)
   analysis_manager = AnalysisManager(game)
   analysis_manager.create_analysis(calculate_intensive_events=True)
-  analysis_manager.write_json_out_to_file(open(f'/tmp/analysis_curr_replay.json', 'w'))
-  with gzip.open(os.path.join('/tmp/output.gzip'), 'wb') as fo:
-    analysis_manager.write_pandas_out_to_file(fo)
-  return _json
+  with gzip.open(os.path.join('/tmp/output.json'), 'wb') as fo:
+    fo.write(analysis_manager.get_json_data())
+  # analysis_manager.write_json_out_to_file(open(f'/tmp/analysis_curr_replay_{fname.split(".")[0]}.json', 'w'))
+  # with gzip.open(os.path.join('/tmp/output.gzip'), 'wb') as fo:
+  #   return analysis_manager.get_json_data(fo)
+  #
+  # return _json
 
 #
-# if __name__=='__main__':
-  # analyzeGame()
+if __name__=='__main__':
+  pass

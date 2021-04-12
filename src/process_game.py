743ce138611805f2ca6524b1e57a864b4fa14193
corbfon <corbin.fonville@gmail.com>
2021-04-11 22:07:14 -0500

    output json rather than file

--- src/process_game.py
+++ src/process_game.py
@@ -7,19 +7,12 @@ from carball.json_parser.game import Game
 from carball.analysis.analysis_manager import AnalysisManager
 
 def analyzeGame(fname):
-  # _json = decompile_replay(f"/tmp/curr_replay_{fname}")
   _json = decompile_replay(fname)
   game = Game()
   game.initialize(loaded_json=_json)
   analysis_manager = AnalysisManager(game)
   analysis_manager.create_analysis(calculate_intensive_events=True)
-  with gzip.open(os.path.join('/tmp/output.json'), 'wb') as fo:
-    fo.write(analysis_manager.get_json_data())
-  # analysis_manager.write_json_out_to_file(open(f'/tmp/analysis_curr_replay_{fname.split(".")[0]}.json', 'w'))
-  # with gzip.open(os.path.join('/tmp/output.gzip'), 'wb') as fo:
-  #   return analysis_manager.get_json_data(fo)
-  #
-  # return _json
+  return analysis_manager.get_json_data()
 
 #
 if __name__=='__main__':

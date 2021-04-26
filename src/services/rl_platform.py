3ced814bf33e438551d2aa5bc82d51b477fa448c
corbfon <corbin.fonville@gmail.com>
2021-04-25 21:59:13 -0500

    remove logs

--- src/services/rl_platform.py
+++ src/services/rl_platform.py
@@ -12,4 +12,6 @@ def send_event(event):
     }
 	request_url = f'{platform_url}/v2/events'
 	response = requests.post(url=request_url, data=json.dumps(event), headers=headers)
+	if response.status_code != 200:
+		raise ValueError(f'Received a non-200 response code from the call to the RL platform: {response.status_code}')
 	return response
